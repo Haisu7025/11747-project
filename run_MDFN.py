6f6c1530512b36fadbae3487c171bef01c4cb1b5
yifan-chen <yifan.chen@outlook.com>
2021-05-06 13:18:16 -0400

    fix save functionality

--- run_MDFN.py
+++ run_MDFN.py
@@ -1102,18 +1102,6 @@ def main():
 
                     global_step += 1
 
-                # Save a trained model, configuration and tokenizer
-                # model_to_save = model.module if hasattr(model,
-                #                                         'module') else model  # Only save the model it-self
-                #
-                # # If we save using the predefined names, we can load using `from_pretrained`
-                # output_model_file = os.path.join(args.output_dir,
-                #                                  str(epoch) + "_" + WEIGHTS_NAME)
-                # output_config_file = os.path.join(args.output_dir, CONFIG_NAME)
-                #
-                # torch.save(model_to_save.state_dict(), output_model_file)
-                # model_to_save.config.to_json_file(output_config_file)
-                # tokenizer.save_vocabulary(args.output_dir)
                 is_nan = False
                 if step % 500 == 0:
                     model.eval()
@@ -1173,6 +1161,19 @@ def main():
                 if is_nan:
                     print("Epoch {} step {} evaluation: NaN happens".format(epoch, step))
                     # writer.write("%s = %s\n" % (key, str(result[key])))
+
+            # Save a trained model, configuration and tokenizer
+            model_to_save = model.module if hasattr(model,
+                                                    'module') else model  # Only save the model it-self
+
+            # If we save using the predefined names, we can load using `from_pretrained`
+            output_model_file = os.path.join(args.output_dir,
+                                             str(epoch) + "_" + WEIGHTS_NAME)
+            output_config_file = os.path.join(args.output_dir, CONFIG_NAME)
+
+            torch.save(model_to_save.state_dict(), output_model_file)
+            model_to_save.config.to_json_file(output_config_file)
+            tokenizer.save_vocabulary(args.output_dir)
 
 
 if __name__ == "__main__":
